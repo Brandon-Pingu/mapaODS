0619d22fe1154e70681f22a9d4fa9fc13f35d8e7
Brandon <brans.ntr@gmail.com>
2020-12-02 15:54:44 -0600

    cortina con botones de ods y controles de + y -

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnJhbmRwaW5ndSIsImEiOiJja2gyZHVsenYwMG1xMnZvNHlwaGplc3U4In0.RxD6xt-IussbHBnjQJ24RQ';
 const spreadSheetId = '1lk6UaqJEHJf_u7CMvOvfFYCVSUyZRQxbrKAhV8i3OQs';
 const url = `https://script.google.com/macros/s/AKfycbw_8waQ6ZMQDpjnSF_GsrdIaVncOVKBr9Ld23O2p3_PYncFaIvR/exec?spreadsheetId=${spreadSheetId}&sheet=marcadores`;
+
 let marcadores = [];
 let btnsFiltros = document.getElementsByClassName('filtro');
+
 const coloresMarcadores = [
-{ods: 'Fin de la pobresa', img: 'assets/MApa ODS-03.png'},
+{ods: 'Fin de la pobreza', img: 'assets/MApa ODS-03.png'},
 {ods: 'Hambre cero', img: 'assets/MApa ODS-04.png'},
 {ods: 'Salud y bienestar', img: 'assets/MApa ODS-05.png'},
 {ods: 'Educación de calidad', img: 'assets/MApa ODS-06.png'},
-{ods: 'Igualdad de genero', img: 'assets/MApa ODS-07.png'},
+{ods: 'Igualdad de género', img: 'assets/MApa ODS-07.png'},
 {ods: 'Agua limpia y saneamiento', img: 'assets/MApa ODS-08.png'},
 {ods: 'Energía asequible y no contaminante', img: 'assets/MApa ODS-09.png'},
-{ods: 'Trabajo decente y crecimiento economico', img: 'assets/MApa ODS-10.png'},
-{ods: 'Industria, inovación e infrestructura', img: 'assets/MApa ODS-11.png'},
+{ods: 'Trabajo decente y crecimiento económico', img: 'assets/MApa ODS-10.png'},
+{ods: 'Industria, inovación e infraestructura', img: 'assets/MApa ODS-11.png'},
 {ods: 'Reducción de las desigualdades', img: 'assets/MApa ODS-12.png'},
 {ods: 'Ciudades y comunidades sostenibles', img: 'assets/MApa ODS-13.png'},
 {ods: 'Producción, consumo y responsabilidades', img: 'assets/MApa ODS-14.png'},
@@ -29,16 +31,17 @@ let map = new mapboxgl.Map({
     center: [-99.104542, 19.628777],
     zoom: 15
 });
-
-// botones de filtros
-document.getElementById('todos').addEventListener('click', () => {
-	mostrarTodosLosMarcadores();
-});
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
 for(let btn of btnsFiltros){
 	btn.addEventListener('click', () => {
-		let texto = btn.innerHTML;
+		let btnId = btn.id.substring(1, 3);
+		let texto = coloresMarcadores[btnId].ods;
 		mostrarTodosLosMarcadores(texto);
+
+		let cortina = document.getElementById('cortina');
+		cortina.style.animation = 'slide .7s ease 1';
+		cortina.style.top = '-110%';
 	});
 }
 
@@ -91,4 +94,4 @@ const mostrarTodosLosMarcadores = (filtro) => {
 	});
 }
 
-mostrarTodosLosMarcadores();
+mostrarTodosLosMarcadores("ninguno");
